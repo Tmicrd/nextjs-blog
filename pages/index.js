728687e73c4856c7013128b77d72aafc283ea029
nancy <liyingxiong23@gmail.com>
2023-03-12 00:23:49 +1100

    updated

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>This is my first Next.js app.</p>
+        <p>I am a genius.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
