48d060f9f8cf585e44f30d94852bf71c2d1cd913
nancy <liyingxiong23@gmail.com>
2023-03-19 18:09:46 +1100

    Updated Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import AppHead from "../components/AppHead";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AppHead />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
@@ -8,9 +7,6 @@ import Date from "../components/date";
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
       <section className={utilStyles.headingMd}>
         <p>I am a genius.</p>
         <p>
